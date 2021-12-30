37e213f22c3a2e5df9b00a660366f59551dee5f3
Sai Lekkala <sailekkala502@gmail.com>
2021-12-30 11:42:49 +0530

    cros enabled

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 const db = require("./models");
 const dbConfig = require("./config/db.config");
 
-app.use(cors());
-app.options('*', cors());
+
 let corsOptions = {
     origin: [
         "http://localhost:4200",
@@ -43,6 +42,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+ });
+
+ app.use(cors());
+// app.options('*', cors());
 
 app.get("/", (req, res) => {
     res.json({
